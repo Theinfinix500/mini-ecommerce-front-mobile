424be70e0fa435e3394dfebcf9d00580171bb1ae
Djebara Abdelali <jbaraali18@gmail.com>
2024-09-19 18:37:52 +0100

    feat: add product to cart

--- src/app/pages/products/components/product-card/product-card.component.ts
+++ src/app/pages/products/components/product-card/product-card.component.ts
@@ -1,6 +1,12 @@
 import { CurrencyPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  Input,
+} from '@angular/core';
 import { Product } from '@core/models';
+import { CartService } from '../../../cart/services/cart.service';
 
 @Component({
   selector: 'app-product-card',
@@ -11,5 +17,11 @@ import { Product } from '@core/models';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductCardComponent {
+  cartService: CartService = inject(CartService);
+
   @Input() product!: Product;
+
+  addProductToCart() {
+    this.cartService.addProductToCart(this.product);
+  }
 }
