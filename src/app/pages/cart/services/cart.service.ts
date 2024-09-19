afcd83f364d5583294981e75bd369c7de30f6ed1
Djebara Abdelali <jbaraali18@gmail.com>
2024-09-19 18:55:58 +0100

    refactor: clean imports

--- src/app/pages/cart/services/cart.service.ts
+++ src/app/pages/cart/services/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CartItem } from '@core/models/cart.model';
 import { Product } from '@core/models';
